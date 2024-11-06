a884e2b255f8ac25db66f4a5b0ccba43176b819e
Abenezer Amare <abenezerame@gmail.com>
2024-11-06 11:02:17 +0300

    Add shadcn ui and some components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <div>
-      <div className="flex justify-between items-center mt-14 mx-16 font-DMSerif">
+      <div className="flex justify-between items-center mt-10 mx-16 font-DMSerif">
         <h1 className="text-2xl md:text-3xl lg:text-4xl  font-semibold">
           <span className="text-primary">Financial</span> Freedom
         </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        text: "#040316",
-        background: "#fbfbfe",
-        primary: "#2f27ce",
-        secondary: "#dddbff",
-        accent: "#443dff",
-      },
-      fontFamily: {
-        DMSerif: ["DM Serif Display", "serif"],
-        DMSans: ["DM Sans", "sans-serif"],
-      },
-    },
+  	extend: {
+  		colors: {
+  			text: '#040316',
+  			background: '#fbfbfe',
+  			primary: '#2f27ce',
+  			secondary: '#dddbff',
+  			accent: '#443dff'
+  		},
+  		fontFamily: {
+  			DMSerif: ["DM Serif Display", "serif"],
+  			DMSans: ["DM Sans", "sans-serif"]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 
